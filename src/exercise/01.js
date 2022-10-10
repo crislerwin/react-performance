d26cb7ffa645ca61e7268c2305c808314fa1dd39
crislerwin <crisler.silva@fael.alunos.com.br>
2022-10-10 08:29:56 -0300

    ✅ webpack magic comments

--- src/exercise/01.js
+++ src/exercise/01.js
@@ -6,8 +6,8 @@ import * as React from 'react'
 
 // 🐨 use React.lazy to create a Globe component which uses a dynamic import
 // to get the Globe component from the '../globe' module.
-const loadGlobe = () => import('../globe')
-const Globe = React.lazy(loadGlobe)
+
+const Globe = React.lazy(() => import(/* webpackPrefetch: true */ '../globe'))
 
 function App() {
   const [showGlobe, setShowGlobe] = React.useState(false)
@@ -27,11 +27,7 @@ function App() {
         padding: '2rem',
       }}
     >
-      <label
-        style={{marginBottom: '1rem'}}
-        onMouseEnter={loadGlobe}
-        onFocus={loadGlobe}
-      >
+      <label style={{marginBottom: '1rem'}}>
         <input
           type="checkbox"
           checked={showGlobe}
